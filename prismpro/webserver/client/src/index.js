d58959578eb6cf92049fa1b670fa00bc1bd2ee81
Sarah Hernandez <sarah.hernandez@nutanix.com>
2019-09-25 22:10:02 -0700

    Begin supporting allowing different paths to trigger different workflows

--- prismpro/webserver/client/src/index.js
+++ prismpro/webserver/client/src/index.js
@@ -2,4 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/**
+ * Handle any hash changes and rerender.
+ */
+function handleNewHash() {
+  const path = window.location.pathname;
+  ReactDOM.render(<App path={ path }/>, document.getElementById('root'));
+}
+
+// Handle the initial route and browser navigation events
+handleNewHash();
+window.addEventListener('hashchange', handleNewHash, false);
