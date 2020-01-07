04c244553bf21dd42d483869ca26cf3b394f8d9a
Sarah Hernandez <sarah.hernandez@nutanix.com>
2020-01-07 15:36:30 -0800

    Update to include localStorage flag automatically set for test drive. Make sure PE index.html also has the scripts injected

--- prismpro/webserver/prismpro-webserver.js
+++ prismpro/webserver/prismpro-webserver.js
@@ -318,6 +318,9 @@ app.get('/', function(req, resp) {
     resp.redirect('/console/');
   }
 });
+var walkmeScript = '<script>!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";analytics.load("SQBKbPgLzflz5eUSkuu0ePSAwKYmiKZ1");analytics.page();}}();</script>'
+var localStorageScript = '<script>if (localStorage) { localStorage.setItem("nutanix_show_search_tutorial", false); localStorage.setItem("FirstTimeExpAutoShown", true) }</script>'
+
 // Serve the static UI files from the proxyHost
 app.get(/console/, function(req, resp) {
   if (!PC_IP || !PC_UI_PASS || !PC_UI_USER) {
@@ -326,13 +329,21 @@ app.get(/console/, function(req, resp) {
     return;
   }
   if (env.enableWalkme) {
-    if (req.url === '/console' || req.url === '/console/' || req.url === '/console/index.html') {
+    if (req.url.indexOf('console/index.html') > -1) {
       // Intercept the index.html file to include a script for walkme
-      var walkmeScript = '<script>!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";analytics.load("SQBKbPgLzflz5eUSkuu0ePSAwKYmiKZ1");analytics.page();}}();</script>'
+      r.get(origHostPortUrl + req.url, function(error, response, body) {
+        if (body) {
+          var position = body.indexOf('</body>');
+          body = [body.slice(0, position), walkmeScript + localStorageScript, body.slice(position)].join('');
+        }
+        resp.send(body);
+      });
+    } else if (req.url === '/console' || req.url === '/console/') {
+      // Intercept the index.html file to include a script for walkme
       r.get(origHostPortUrl + '/console/index.html', function(error, response, body) {
         if (body) {
           var position = body.indexOf('</body>');
-          body = [body.slice(0, position), walkmeScript, body.slice(position)].join('');
+          body = [body.slice(0, position), walkmeScript + localStorageScript, body.slice(position)].join('');
         }
         resp.send(body);
       });
